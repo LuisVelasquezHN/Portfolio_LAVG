db2a15adde370c12b9817a31a01f323db3ede53b
lavg98 <110047971+lavg98@users.noreply.github.com>
2025-02-26 18:39:05 -0600

    Update ScrollDownIndicator.jsx

--- src/components/ScrollDownIndicator.jsx
+++ src/components/ScrollDownIndicator.jsx
@@ -13,7 +13,7 @@ export const ScrollDownIndicator = () => {
     }, []);
 
     return (
-        <div className={`absolute bottom-10 left-1/2 transform -translate-x-1/2 transition-opacity duration-300`}>
+        <div className={`absolute bottom-10 left-1/2 transform -translate-x-1/2 transition-opacity duration-300 ${isVisible ? "opacity-100" : "opacity-0"}`}>
             <a href="#about" className="flex flex-col items-center cursor-pointer">
                 <span className="text-sm text-gray-400">Scroll Down</span>
                 <div className="w-5 h-8 border-2 border-gray-500 rounded-full flex justify-center items-start relative mt-2">
