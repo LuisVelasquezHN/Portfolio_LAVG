66f53fe323933eaeb0946d22dbef7f2902a7d35d
lavg98 <110047971+lavg98@users.noreply.github.com>
2025-03-13 00:31:49 -0600

    Update OtrosProyectos.jsx

--- src/pages/OtrosProyectos.jsx
+++ src/pages/OtrosProyectos.jsx
@@ -29,35 +29,35 @@ export const OtrosProyectos = () => {
             name: "Portal de Empleados",
             portada: "projects/otros/portalProject.png",
             descripcion: "El Portal de Empleados es una plataforma interna diseñada para optimizar el acceso a información y recursos clave dentro de la empresa Cable Color. Permite a los colaboradores explorar enlaces directos a distintos sistemas empresariales, acceder al directorio de empleados para encontrar información de contacto, visualizar fotografías de eventos corporativos, enviar sugerencias a la empresa y generar firmas de correo electrónico de manera automatizada. Desarrollado con PHP y servicios API en Oracle APEX, este portal mejora la comunicación y eficiencia dentro de la organización.",
-            tecnologias: [<SVGphp/>, <SVGoracle/> ]
+            tecnologias: [<SVGphp />, <SVGoracle />]
         },
         {
             id: 2,
             name: "Gestor Web",
             portada: "projects/otros/gestorProject.png",
             descripcion: "El Gestor Web es una herramienta de administración de contenido que permite gestionar dinámicamente diversas secciones de sitios web a nivel regional, incluyendo el Portal de Colaboradores. A través de esta plataforma, es posible modificar imágenes, textos, íconos, títulos y secciones de servicios, tanto residenciales como corporativos, además de gestionar información de agencias. Desarrollado íntegramente en Oracle APEX, este sistema ofrece una solución eficiente y centralizada para la gestión web empresarial.",
-            tecnologias: [<SVGoracle/>]
+            tecnologias: [<SVGoracle />]
         },
         {
             id: 3,
             name: "DMS (Document Management System)",
             portada: "projects/otros/dmsProject.png",
             descripcion: "El Sistema de Gestión Documental (DMS) es una plataforma diseñada para el manejo de documentos confidenciales dentro del área de Procesos. Facilita la administración de políticas empresariales, la aprobación de documentos mediante un sistema de tickets y la comunicación entre áreas clave, como gerencia y jefaturas. Mi aporte en este proyecto incluyó el desarrollo de un tablero Kanban interactivo para gestionar solicitudes, facilitando la trazabilidad de documentos y la colaboración en tiempo real. Además, implementé la funcionalidad de mantenimiento y aprobación de documentos, automatizando la generación de versiones. Proyecto desarrollado con Angular y Node.js.",
-            tecnologias: [<SVGangular/>, <SVGNode/>]
+            tecnologias: [<SVGangular />, <SVGNode />]
         },
         {
             id: 4,
             name: "Sistema Talento Humano",
             portada: "projects/otros/thProject.png",
             descripcion: "El Sistema de Talento Humano es una plataforma integral para la gestión de vacantes, tanto nuevas como de reemplazo. Conectado con sitios web externos y el Portal de Empleados, permite la publicación de ofertas laborales y la referencia de candidatos. Implementé un tablero Kanban que facilita la colaboración entre los jefes que solicitan personal y los especialistas encargados del proceso de selección. También desarrollé un módulo de documentación para candidatos, que permite la carga de documentos a través de un enlace público. Proyecto desarrollado con React y Node.js para el área de Recursos Humanos.",
-            tecnologias: [<SVGreact/>, <SVGNode/>]
+            tecnologias: [<SVGreact />, <SVGNode />]
         },
         {
             id: 5,
             name: "Sistema Dispatch",
             portada: "projects/otros/dispatchProject.png",
-            descripcion: "El Sistema Dispatch permite la organización eficiente de grupos y técnicos encargados de instalaciones en diferentes ubicaciones del país. Facilita la gestión y asignación de órdenes generadas en el sistema, optimizando el trabajo de los gestores y técnicos de campo. Mi aporte incluyó el desarrollo de un sistema para la extracción y generación de órdenes de trabajo, asegurando una asignación eficiente de recursos. Proyecto desarrollado con React y Node.js para el área de Dispatch.",
-            tecnologias: [<SVGreact/>, <SVGNode/>]
+            descripcion: "El Sistema Dispatch permite la organización eficiente de grupos y técnicos encargados de instalaciones en diferentes ubicaciones del país. Facilita la gestión y asignación de órdenes generadas en el sistema, optimizando el trabajo de los gestores y técnicos de campo. Mi aporte incluyó el desarrollo de una sección para la extracción y generación de órdenes de trabajo, asegurando una asignación eficiente de recursos. Proyecto desarrollado con React y Node.js para el área de Dispatch.",
+            tecnologias: [<SVGreact />, <SVGNode />]
         }
     ];
 
@@ -71,7 +71,7 @@ export const OtrosProyectos = () => {
                     ]}
                 />
                 <h1 className='dark:text-white/80 text-gray-600/80 px-8 py-4 text-2xl md:text-3xl'>
-                    Proyectos en los que he colaborado.
+                    Proyectos y Colaboraciones.
                 </h1>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6 p-4">
                     {projects.map((project) => (
@@ -80,7 +80,7 @@ export const OtrosProyectos = () => {
                             className="relative rounded-2xl overflow-hidden shadow-[3px_3px_19px_4px_rgba(0,_0,_0,_0.1)] h-64 flex items-end cursor-pointer opacity-90 group"
                             style={{ backgroundImage: `url(${project.portada})`, backgroundSize: "cover", backgroundPosition: "center" }}
                         >
-                            <div className="absolute inset-0 bg-[#2f97e0] dark:bg-[#031d2f] opacity-50 h-20 md:h-15 mt-auto group-hover:opacity-70 transition-opacity duration-300"></div>
+                            <div className="absolute inset-0 bg-[#2182c7] dark:bg-[#031d2f] opacity-50 h-20 md:h-15 mt-auto group-hover:opacity-70 transition-opacity duration-300"></div>
 
                             <div className="absolute inset-0 flex items-center justify-center cursor-pointer bg-black/20 dark:bg-black/40 text-white text-lg font-semibold rounded-lg md:opacity-0 md:group-hover:opacity-100 transition-opacity duration-300">
                                 <a
