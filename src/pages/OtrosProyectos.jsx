8fa6ca012f12e2e4223e336190ac147a98aedf46
lavg98 <110047971+lavg98@users.noreply.github.com>
2025-03-25 10:39:13 -0600

    depuracion

--- src/pages/OtrosProyectos.jsx
+++ src/pages/OtrosProyectos.jsx
@@ -1,86 +1,12 @@
 import React from 'react';
 import { Footer } from '../components/Footer';
 import { Breadcrumb } from '../components/Breadcrumb';
-// import { IconExternalLink } from '@tabler/icons-react';
-// import { ModalProjects } from '../components/ModalProjects';
-// import { SVGphp } from '../components/SVG/SVGphp';
-// import { SVGoracle } from '../components/SVG/SVGoracle';
-// import { SVGNode } from '../components/SVG/SVGNode';
-// import { SVGreact } from '../components/SVG/SVGreact';
-// import { SVGangular } from '../components/SVG/SVGangular';
 import { useTranslation } from 'react-i18next';
 import { Bentox2 } from './Bentox2';
 
 
 export const OtrosProyectos = () => {
     const { t } = useTranslation();
-
-    // const [selectedProject, setSelectedProject] = useState(null);
-    // const [isModalOpen, setIsModalOpen] = useState(false);
-
-    // const openModal = (project) => {
-    //     setSelectedProject(project);
-    //     setIsModalOpen(true);
-    // };
-
-    // const closeModal = () => {
-    //     setIsModalOpen(false);
-    //     setSelectedProject(null);
-    // };
-
-    // const projects = [
-    //     {
-    //         id: 1,
-    //         name: t('otherProjects.namePortal'),
-    //         portada: "projects/otros/portalProject.png",
-    //         descripcion: t('otherProjects.descripcionPortal'),
-    //         tecnologias: [
-    //             <SVGphp key="php-1" />,
-    //             <SVGoracle key="oracle-1" />
-    //         ]
-    //     },
-    //     {
-    //         id: 2,
-    //         name: t('otherProjects.nameGestor'),
-    //         portada: "projects/otros/gestorProject.png",
-    //         descripcion: t('otherProjects.descripcionGestor'),
-    //         tecnologias: [
-    //             <SVGoracle key="oracle-2" />
-    //         ]
-    //     },
-    //     {
-    //         id: 3,
-    //         name: t('otherProjects.nameDMS'),
-    //         portada: "projects/otros/dmsProject.png",
-    //         descripcion: t('otherProjects.descripcionDMS'),
-    //         tecnologias: [
-    //             <SVGangular key="angular-3" />,
-    //             <SVGNode key="node-3" />
-    //         ]
-    //     },
-    //     {
-    //         id: 4,
-    //         name: t('otherProjects.nameTH'),
-    //         portada: "projects/otros/thProject.png",
-    //         descripcion: t('otherProjects.descripcionTH'),
-    //         tecnologias: [
-    //             <SVGreact key="react-4" />,
-    //             <SVGNode key="node-4" />
-    //         ]
-    //     },
-    //     {
-    //         id: 5,
-    //         name: t('otherProjects.nameDispatch'),
-    //         portada: "projects/otros/dispatchProject.png",
-    //         descripcion: t('otherProjects.descripcionDispatch'),
-    //         tecnologias: [
-    //             <SVGreact key="react-5" />,
-    //             <SVGNode key="node-5" />
-    //         ]
-    //     }
-    // ];
-
-
 
     return (
         <>
@@ -94,55 +20,10 @@ export const OtrosProyectos = () => {
                 <h1 className='dark:text-white/80 text-gray-600/80 px-4 sm:px-8 py-4 text-2xl md:text-3xl'>
                     {t('otherProjects.title')}
                 </h1>
-                {/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6 p-4">
-                    {projects.map((project) => (
-                        <div
-                            key={project.id}
-                            className="relative rounded-2xl overflow-hidden shadow-[3px_3px_19px_4px_rgba(0,_0,_0,_0.1)] min-h-56 md:h-64 flex items-end cursor-pointer opacity-90 group"
-                            style={{ backgroundImage: `url(${project.portada})`, backgroundSize: "cover", backgroundPosition: "center" }}
-                            onClick={() => openModal(project)}
-                        >
-                            <div className="absolute inset-0 bg-gradient-to-t from-black/40 md:from-black/60 dark:from-black/80 to-transparent h-full md:h-full mt-auto group-hover:opacity-70 transition-opacity duration-300"></div>
-
-                            <div className="absolute inset-0 flex items-center justify-center cursor-pointer bg-black/20 dark:bg-black/40 text-white text-lg font-semibold rounded-lg md:opacity-0 md:group-hover:opacity-100 transition-opacity duration-300">
-                            </div>
-
-                            <div className="relative p-4 text-white w-full">
-                                <h3 className="text-lg font-semibold inline-block text-balance">{project.name}
-
-                                    <IconExternalLink stroke={2}
-                                        className="w-5 h-5 text-current inline-block align-middle ml-1" />
-                                </h3>
-                                <p className="text-sm text-gray-600 dark:text-gray-300 flex gap-2 pt-1">
-                                    {project.tecnologias}
-                                </p>
-                            </div>
-                        </div>
-                    ))}
-                </div> */}
             </section>
 
-            {/* <BentoProjects /> */}
             <Bentox2/>
 
-            {/* <ModalProjects
-                isOpen={isModalOpen}
-                onClose={closeModal}
-                modalTitle={selectedProject?.name}
-                data={selectedProject ? [
-                    <div className="flex flex-col gap-4 md:grid md:grid-cols-2 w-full">
-                        <div className='hiddenObs2'>
-                            <h4 className="text-lg font-semibold text-[#515151] dark:text-gray-200">{t('otherProjects.tecnologias')}</h4>
-                            <p className="text-sm text-gray-600 dark:text-gray-300 flex gap-2 pt-1">{selectedProject.tecnologias}</p>
-                            <h4 className="text-lg font-semibold mt-4 text-[#515151] dark:text-gray-200">{t('otherProjects.descripcion')}</h4>
-                            <p className="text-sm text-gray-600 dark:text-gray-300 text-pretty">{selectedProject.descripcion}</p>
-                        </div>
-                        <div className="flex justify-center items-center sm:block md:flex hiddenObs">
-                            <img src={selectedProject.portada} alt={selectedProject.name} className="rounded-2xl overflow-hidden shadow-lg max-w-full h-auto md:h-64 object-cover opacity-90" />
-                        </div>
-                    </div>
-                ] : []}
-            /> */}
             <Footer />
         </>
     );
